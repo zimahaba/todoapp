805ab98e532653bca1e45b8d597dc3701613596d
zimahaba <pmaquitorres@gmail.com>
2023-04-06 15:32:04 -0300

    todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [item, setItem] = useState('');
+  const [items, setItems] = useState([]);
+  const [mouseOverItemIndex, setMouseOverItemIndex] = useState(-1);
+  
+  const addItem = () => {
+    if (item == '') {
+      return;
+    }
+    setItems([...items, {value: item, checked: false}]);
+    setItem('');
+  }
+
+  const addHandler = () => {
+    addItem();
+  }
+
+  const inputKeyDownHandler = (event) => {
+    if (event.keyCode === 13) {
+      addItem();
+    }
+  }
+
+  const removeHandler = (index) => {
+    setItems(items.filter((item, i) => i != index));
+  }
+
+  const onCheckboxClickHandler = (event, index) => {
+    const updatedItems = items.map((item, i) => {
+      if (i === index) {
+        item.checked = event.target.checked;
+      }
+      return item;
+    });
+    setItems(updatedItems);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="card">
+        <h2 className="title">Todo App</h2>
+        <div className="inputContainer">
+          <input className="input" value={item} onKeyDown={inputKeyDownHandler} onChange={(event) => setItem(event.target.value)}></input>
+          <button className="addButton" onClick={addHandler}></button>
+        </div>
+        <ul className="list">
+          {items.map((element, index) => (
+            <li key={index} className="listItem" onMouseOver={() => setMouseOverItemIndex(index)} onMouseOut={() => setMouseOverItemIndex(-1)}>
+              <div className="itemContainer">
+                <div className="labelContainer">
+                  <input type="checkbox" checked={element.checked} onChange={(event) => onCheckboxClickHandler(event, index)}></input>
+                  <label>{element.value}</label>
+                </div>
+                <button onClick={() => removeHandler(index)} className={mouseOverItemIndex === index ? 'removeButtonShown' : 'removeButtonHidden'}></button>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div>
+          You have {items.filter(item => !item.checked).length} pending tasks.
+        </div>
+      </div>
     </div>
   );
 }
